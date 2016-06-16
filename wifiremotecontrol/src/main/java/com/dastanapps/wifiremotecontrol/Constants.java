ef42a8b21c249e1ecc9b10fa15ef6732f8c683d6
iqbalMkart <iqbal.ahmed@mebelkart.com>
2016-06-16 18:17:49 +0530

    Added wifiremote control code in seperate app

---
+++ wifiremotecontrol/src/main/java/com/dastanapps/wifiremotecontrol/Constants.java
@@ -0,0 +1,13 @@
+package com.dastanapps.wifiremotecontrol;
+
+/**
+ * Created by Sourabh Soni on 07-03-2015.
+ */
+public class Constants {
+    public static final String SERVER_IP = "192.168.0.106";
+    public static final int SERVER_PORT = 8998;
+    public static final String PLAY="play";
+    public static final String NEXT="next";
+    public static final String PREVIOUS="previous";
+    public static final String MOUSE_LEFT_CLICK="left_click";
+}
